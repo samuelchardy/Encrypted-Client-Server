d009ddce1acc70baaf4866c091147bbaa0d23232
Hardy <hardys3@egb074000022.lancs.local>
2020-01-21 14:32:08 +0000

    Added password complexity checker to the server.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+from password_strength import PasswordStats
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -10,9 +11,12 @@ serverSocket.listen(5)
 print("Server Active")
 
 while True:
-    clientSocket,addr = serverSocket.accept()      
-    print("Connected to: %s" % str(addr))
-    
-    msg = "Connected to " + str(host) + ":" + str(port) + "\r\n" 
-    clientSocket.send(msg.encode('ascii'))
-    clientSocket.close()
+	clientSocket,addr = serverSocket.accept()      
+	print("Connected to: %s" % str(addr))
+
+	passStats = PasswordStats("");
+	print(passStats.strength());
+
+	msg = "Connected to " + str(host) + ":" + str(port) + "\r\n" 
+	clientSocket.send(msg.encode('ascii'))
+	clientSocket.close()
