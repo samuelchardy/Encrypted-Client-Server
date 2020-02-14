33630e8835d94645c629ec970ebf2d019dee6cf5
Jack <EliteSpud@users.noreply.github.com>
2020-02-14 14:57:38 +0000

    Revert "reset to working version."
    This reverts commit bcbd76a90009d641c218a5e8afc8a1bcc0203590.

--- server.py
+++ server.py
@@ -160,8 +160,7 @@ class Server():
         if attempts<0:
           self.timeoutLogin(self.username)
           if not self.checkLogins(self.username):
-            emailMsg = "The user: " + self.username + " has failed to login 9 times."
-            self.alertAdmin(emailMsg)
+            self.alertAdmin(self.username)
           attempts+=1
           delay=60
           for i in range(delay):
@@ -218,8 +217,8 @@ class Server():
       mc.execute("INSERT INTO personalinfo(Username,Email,Surname,Forename,DOB) VALUES(%s,%s,%s,%s,%s)",(username,email,surname,forename,dob))
       mc.execute("SELECT UserID from personalinfo where Username = '"+username+"'")
       results = mc.fetchall()
-      userID = results[0][0]       
-      print("your ID Happens to be " + str(userID))
+      userID = results[0][0] 
+      print("your ID Happens to be " + userID)
       mc.execute("INSERT INTO roles(UserID,RoleID) Values(%s,%s)",(userID,0))
       c.commit()
       c.close()	  
@@ -266,7 +265,7 @@ class Server():
       return clear
 
 
-    def alertAdmin(self, text):
+    def alertAdmin(self, username):
       print("Alerting sys admins of suspicious behaviour")
       #@title Email OTP to Recipient
       c = self.server.DB()
@@ -291,7 +290,7 @@ class Server():
       message["From"] = address
       message["To"] = ", ".join(recipients)
 
-      #text = "The user: " + username + " has failed to login 9 times. Please execute order 66."
+      text = "The user: " + username + " has failed to login 9 times. Please execute order 66."
 
       msgText = MIMEText(text, "plain")
       message.attach(msgText)
@@ -314,25 +313,24 @@ class Server():
       mc = c.cursor()
       mc.execute("SELECT LastValidation from login where username = '"+user+"'")
       insertion_date = mc.fetchall()
-      if mc.rowcount>0:
-        dt = datetime.now()
-        
-        insertion_date = datetime.strptime(str(insertion_date[0][0]), "%Y-%m-%d %H:%M:%S")
-        cTime =dt.strftime("%Y-%m-%d %H:%M:%S")
-        cTime = datetime.strptime(str(cTime), "%Y-%m-%d %H:%M:%S")
-        print(cTime)
-        print(insertion_date)
-        time_between_insertion = cTime - insertion_date
+      dt = datetime.now()
+      
+      insertion_date = datetime.strptime(str(insertion_date[0][0]), "%Y-%m-%d %H:%M:%S")
+      cTime =dt.strftime("%Y-%m-%d %H:%M:%S")
+      cTime = datetime.strptime(str(cTime), "%Y-%m-%d %H:%M:%S")
+      print(cTime)
+      print(insertion_date)
+      time_between_insertion = cTime - insertion_date
 
-        if  time_between_insertion.days>14:
-          print ("The insertion date is older than 14 days")
-          return True
-          mc.execute("update login set LastValidation = %s where username = %s",(cTime, user))
-          c.commit()
-        else:
-          print ("The insertion date is not older than 14 days")
-          return False
-      return False
+      if  time_between_insertion.days>14:
+        print ("The insertion date is older than 14 days")
+        return True
+        mc.execute("update login set LastValidation = %s where username = %s",(cTime, user))
+        c.commit()
+      else:
+        print ("The insertion date is not older than 14 days")
+        return False
+
 
 
     def run(self):
@@ -385,7 +383,7 @@ class Server():
           time.sleep(30)
           for method in methods:
             methodsStr = methodsStr + method + ","
-          print("METHOD STRING : ",methodsStr)
+
           cliMsg = messageParser.make(self.server.parser, self.c, clientPublicKey, "1", methodsStr)
           self.clientsocket.send(cliMsg)
           #just need to send these back to the user now
